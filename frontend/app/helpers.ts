b69b397ba61d43820e390dfd2528594eeecc3646
Abdulrasaq Nasirudeen <nosisky@gmail.com>
2024-10-20 21:52:47 +0100

    Add more logs

--- frontend/app/helpers.ts
+++ frontend/app/helpers.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 
-console.log(process.env.NEXT_PUBLIC_API_URL, "API URL");
+console.log(
+  process.env.NEXT_PUBLIC_API_URL,
+  "API URL",
+  process.env.NEXT_PUBLIC_API_URL
+);
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
